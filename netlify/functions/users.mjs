c07663dd2484b6d7bf5e526ddd7ef0999fb07880
Mirna Marinho Andrade <amimarinhoux@gmail.com>
2025-11-04 23:11:35 -0300

    fix: ajusta parse do response

--- netlify/functions/users.mjs
+++ netlify/functions/users.mjs
@@ -1,5 +1,9 @@
 import users from "../../users.json";
 
 export default async () => {
-  return new Response(JSON.parse(users));
+  return new Response(JSON.stringify(users), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
